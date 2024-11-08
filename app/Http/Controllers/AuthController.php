26c1fcafcd24d04d28e319cc593741c9dab7efd8
Lucas França <lucasvpfranca@gmail.com>
2024-11-08 02:18:07 -0300

    Add method register in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
 
 class AuthController extends Controller
 {
+    public function register(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name'=> 'string|required|min:3|max:100',
+            'email'=>'string|required|email|unique:users',
+            'password'=> 'string|required|min:7'
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+
+                'message'=> 'Erro de validação',
+                'errors'=> $validator->errors()
+            ],400);
+        }
+
+        try {
+            $user = User::create([
+                'name'=> $request->name,
+                'email'=> $request->email,
+                'password'=> $request->password
+            ]);
+
+            $token = $user->createToken('app_token')->plainTextToken;
+        }catch(\Exception $e) {
+            return response()->json([
+                'message' => 'Erro ao  registrar um usuárip',
+                'error'=> $e->getMessage()
+            ]);
+        }
+
+
+        return response()->json([
+            'message' => 'usuário registrado com sucesso!',
+            'user'=>$user,
+            'token' => $token
+        ]);
+
+
+    }
     //
 }
